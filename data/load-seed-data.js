fcbd4656cf24ead914d2b1005adc5719ab620fcd
Julianne Vela <cjvela@outlook.com>
2021-02-20 20:50:13 -0800

    fixed final reference errors

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -32,18 +32,20 @@ async function run() {
                         name, 
                         nasa_jpl_url, 
                         absolute_magnitude_h, 
-                        estimated_diameter, 
+                        estimated_diameter_min,
+                        estimated_diameter_max, 
                         is_potentially_hazardous_asteroid, 
                         is_sentry_object, 
                         owner_id)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7);
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
                 `,
         [
           neo.neo_reference_id, 
           neo.name, 
           neo.nasa_jpl_url, 
           neo.absolute_magnitude_h, 
-          neo.estimated_diameter, 
+          neo.estimated_diameter_min,
+          neo.estimated_diameter_max, 
           neo.is_potentially_hazardous_asteroid, 
           neo.is_sentry_object, 
           user.id
