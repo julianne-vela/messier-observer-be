4730e5d2aef3bea958acfb28e7484a319c5af709
Julianne Vela <cjvela@outlook.com>
2021-02-20 22:11:39 -0800

    second test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 require('dotenv').config();
 
 const { execSync } = require('child_process');
@@ -35,151 +36,205 @@ describe('app routes', () => {
 
       const expectation = [
         {
-          'neo_reference_id': '2005879',
-          'name': '5879 Almeria 1992 CH1',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2005879',
-          'absolute_magnitude_h': 17.6,
-          'estimated_diameter_min': 0.8027031673,
-          'estimated_diameter_max': 1.7948988478,  'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 1,
+          "neo_reference_id": 2005879,
+          "name": "5879 Almeria 1992 CH1",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2005879",
+          "absolute_magnitude_h": "17.600",
+          "estimated_diameter_min": "0.802703167300",
+          "estimated_diameter_max": "1.794898847800",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '2489235',
-          'name': '489235 2006 QA58',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2489235',
-          'absolute_magnitude_h': 20.4,
-          'estimated_diameter_min': 0.2210828104,
-          'estimated_diameter_max': 0.4943561926,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 2,
+          "neo_reference_id": 2489235,
+          "name": "489235 2006 QA58",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2489235",
+          "absolute_magnitude_h": "20.400",
+          "estimated_diameter_min": "0.221082810400",
+          "estimated_diameter_max": "0.494356192600",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '2512234',
-          'name': '512234 2015 VO66',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2512234',
-          'absolute_magnitude_h': 20.5,
-          'estimated_diameter_min': 0.2111324448,
-          'estimated_diameter_max': 0.4721064988,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 3,
+          "neo_reference_id": 2512234,
+          "name": "512234 2015 VO66",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2512234",
+          "absolute_magnitude_h": "20.500",
+          "estimated_diameter_min": "0.211132444800",
+          "estimated_diameter_max": "0.472106498800",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '3744324',
-          'name': '2016 CD248',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3744324',
-          'absolute_magnitude_h': 22.2,
-          'estimated_diameter_min': 0.096506147,
-          'estimated_diameter_max': 0.2157943048,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 4,
+          "neo_reference_id": 3744324,
+          "name": "2016 CD248",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3744324",
+          "absolute_magnitude_h": "22.200",
+          "estimated_diameter_min": "0.096506147000",
+          "estimated_diameter_max": "0.215794304800",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '3759724',
-          'name': '2016 SA2',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3759724',
-          'absolute_magnitude_h': 28.2,
-          'estimated_diameter_min': 0.0060891262,
-          'estimated_diameter_max': 0.0136157002,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 5,
+          "neo_reference_id": 3759724,
+          "name": "2016 SA2",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3759724",
+          "absolute_magnitude_h": "28.200",
+          "estimated_diameter_min": "0.006089126200",
+          "estimated_diameter_max": "0.013615700200",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '3766594',
-          'name': '2017 BY',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3766594',
-          'absolute_magnitude_h': 25.5,
-          'estimated_diameter_min': 0.0211132445,
-          'estimated_diameter_max': 0.0472106499,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 6,
+          "neo_reference_id": 3766594,
+          "name": "2017 BY",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3766594",
+          "absolute_magnitude_h": "25.500",
+          "estimated_diameter_min": "0.021113244500",
+          "estimated_diameter_max": "0.047210649900",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54016468',
-          'name': '2020 GJ1',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54016468',
-          'absolute_magnitude_h': 26.171,
-          'estimated_diameter_min': 0.0155008308,
-          'estimated_diameter_max': 0.0346609114,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 7,
+          "neo_reference_id": 54016468,
+          "name": "2020 GJ1",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54016468",
+          "absolute_magnitude_h": "26.171",
+          "estimated_diameter_min": "0.015500830800",
+          "estimated_diameter_max": "0.034660911400",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54051321',
-          'name': '2020 QZ3',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54051321',
-          'absolute_magnitude_h': 21.171,
-          'estimated_diameter_min': 0.1550083083,
-          'estimated_diameter_max': 0.3466091145,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 8,
+          "neo_reference_id": 54051321,
+          "name": "2020 QZ3",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54051321",
+          "absolute_magnitude_h": "21.171",
+          "estimated_diameter_min": "0.155008308300",
+          "estimated_diameter_max": "0.346609114500",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54103332',
-          'name': '2020 YY4',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54103332',
-          'absolute_magnitude_h': 20.746,
-          'estimated_diameter_min': 0.1885189346,
-          'estimated_diameter_max': 0.4215411527,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 9,
+          "neo_reference_id": 54103332,
+          "name": "2020 YY4",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54103332",
+          "absolute_magnitude_h": "20.746",
+          "estimated_diameter_min": "0.188518934600",
+          "estimated_diameter_max": "0.421541152700",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54110065',
-          'name': '2021 CD1',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54110065',
-          'absolute_magnitude_h': 24.102,
-          'estimated_diameter_min': 0.0401934214,
-          'estimated_diameter_max': 0.0898752225,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 10,
+          "neo_reference_id": 54110065,
+          "name": "2021 CD1",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54110065",
+          "absolute_magnitude_h": "24.102",
+          "estimated_diameter_min": "0.040193421400",
+          "estimated_diameter_max": "0.089875222500",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54110075',
-          'name': '2021 CQ1',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54110075',
-          'absolute_magnitude_h': 21.639,
-          'estimated_diameter_min': 0.1249553849,
-          'estimated_diameter_max': 0.2794087348,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 11,
+          "neo_reference_id": 54110075,
+          "name": "2021 CQ1",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54110075",
+          "absolute_magnitude_h": "21.639",
+          "estimated_diameter_min": "0.124955384900",
+          "estimated_diameter_max": "0.279408734800",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54111261',
-          'name': '2021 CA4',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54111261',
-          'absolute_magnitude_h': 24.45,
-          'estimated_diameter_min': 0.0342416731,
-          'estimated_diameter_max': 0.0765667087,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 12,
+          "neo_reference_id": 54111261,
+          "name": "2021 CA4",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54111261",
+          "absolute_magnitude_h": "24.450",
+          "estimated_diameter_min": "0.034241673100",
+          "estimated_diameter_max": "0.076566708700",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54117569',
-          'name': '2021 CS6',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54117569',
-          'absolute_magnitude_h': 27.756,
-          'estimated_diameter_min': 0.0074705913,
-          'estimated_diameter_max': 0.0167047499,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 13,
+          "neo_reference_id": 54117569,
+          "name": "2021 CS6",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54117569",
+          "absolute_magnitude_h": "27.756",
+          "estimated_diameter_min": "0.007470591300",
+          "estimated_diameter_max": "0.016704749900",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         },
         {
-          'neo_reference_id': '54117596',
-          'name': '2021 CW7',
-          'nasa_jpl_url': 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54117596',
-          'absolute_magnitude_h': 31.207,
-          'estimated_diameter_min': 0.0015245967,
-          'estimated_diameter_max': 0.0034091019,
-          'is_potentially_hazardous_asteroid': false,
-          'is_sentry_object': false
+          "id": 14,
+          "neo_reference_id": 54117596,
+          "name": "2021 CW7",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54117596",
+          "absolute_magnitude_h": "31.207",
+          "estimated_diameter_min": "0.001524596700",
+          "estimated_diameter_max": "0.003409101900",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
         }
       ];
-
+      
       const data = await fakeRequest(app)
         .get('/neos')
         .expect('Content-Type', /json/)
         .expect(200);
+      
+      expect(data.body).toEqual(expectation);
+    });
 
+    test('returns an individual neo with the matching neo_reference_id', async() => {
+
+      const expectation = [
+        {
+          "id": 13,
+          "neo_reference_id": 54117569,
+          "name": "2021 CS6",
+          "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=54117569",
+          "absolute_magnitude_h": "27.756",
+          "estimated_diameter_min": "0.007470591300",
+          "estimated_diameter_max": "0.016704749900",
+          "is_potentially_hazardous_asteroid": false,
+          "is_sentry_object": false,
+          "owner_id": 1
+        }
+      ];
+      
+      const data = await fakeRequest(app)
+        .get('/neos/13')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      
       expect(data.body).toEqual(expectation);
     });
   });
