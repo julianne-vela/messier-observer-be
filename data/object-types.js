d2ea5dbf9acebd195f07c70626b7f2e4e0ef86f4
Julianne Vela <cjvela@outlook.com>
2021-02-25 14:39:53 -0800

    fixed setup-db errors with extended dataset

--- data/object-types.js
+++ data/object-types.js
@@ -13,5 +13,61 @@ module.exports = [
     {
         type: 'Supernova remnant',
 
-    }
+    },
+    {
+        type: 'H II region nebula with cluster',
+
+    },
+    {
+        type: 'Milky Way star cloud',
+
+    },
+    {
+        type: 'Planetary nebula',
+
+    },
+    {
+        type: 'Spiral galaxy',
+
+    },
+    {
+        type: 'Dwarf elliptical galaxy',
+
+    },
+    {
+        type: 'Double star',
+
+    },
+    {
+        type: 'H II region nebula',
+
+    },
+    {
+        type: 'H II region nebula (part of the Orion Nebula)',
+
+    },
+    {
+        type: 'Elliptical galaxy',
+
+    },
+    {
+        type: 'Barred Spiral galaxy',
+
+    },
+    {
+        type: 'Asterism',
+
+    },
+    {
+        type: 'Diffuse nebula',
+
+    },
+    {
+        type: 'Starburst galaxy',
+
+    },
+    {
+        type: 'Lenticular galaxy',
+
+    },
 ];
